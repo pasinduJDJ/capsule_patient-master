dcd945551ee29529db72c355ce2f0e42d1407533
Abdul Muksith <106472158+MuksithMRA@users.noreply.github.com>
2023-04-20 23:00:17 +0530

    New Changes

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -25,77 +25,97 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: ScreenSize.width,
-        padding: const EdgeInsets.symmetric(horizontal: 30),
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Form(
-              key: _formKey,
+            Row(children: [
+              Container(
+                width: MediaQuery.of(context).size.width / 1.4,
+                height: MediaQuery.of(context).size.height * 0.4,
+                decoration: const BoxDecoration(
+                    // borderRadius: BorderRadius.circular(80),
+                    image: DecorationImage(
+                        image:
+                            AssetImage("assets/images/blueDesignSignIn.png"))),
+              ),
+            ]),
+            Container(
+              width: ScreenSize.width,
+              padding: const EdgeInsets.symmetric(horizontal: 30),
               child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const TextFormFieldWidget(
-                    label: "Email",
-                  ),
                   const SizedBox(
-                    height: 10,
+                    height: 30,
                   ),
-                  const TextFormFieldWidget(
-                    label: "Password",
-                    isPassword: true,
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        const TextFormFieldWidget(
+                          label: "Email",
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const TextFormFieldWidget(
+                          label: "Password",
+                          isPassword: true,
+                        ),
+                        const SizedBox(height: 10),
+                        CheckBoxWithLabels(
+                          onRememberCheck: ((isChecked) =>
+                              debugPrint(isChecked.toString())),
+                          label: "Remember me",
+                        ),
+                        PrimaryButtonWidget(
+                          ontap: () async {},
+                          height: ScreenSize.height * 0.06,
+                          width: ScreenSize.width,
+                          text: "Sign in",
+                        ),
+                      ],
+                    ),
                   ),
                   const SizedBox(height: 10),
-                  CheckBoxWithLabels(
-                    onRememberCheck: ((isChecked) =>
-                        debugPrint(isChecked.toString())),
-                    label: "Remember me",
+                  const Text(
+                    "OR",
+                    style: TextStyle(fontSize: 14),
                   ),
+                  const SizedBox(height: 10),
                   PrimaryButtonWidget(
-                    ontap: () async {},
+                    ontap: () {},
+                    isOutlined: true,
                     height: ScreenSize.height * 0.06,
                     width: ScreenSize.width,
-                    text: "Sign in",
+                    text: "Sign in with Google",
                   ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              "OR",
-              style: TextStyle(fontSize: 14),
-            ),
-            const SizedBox(height: 10),
-            PrimaryButtonWidget(
-              ontap: () {},
-              isOutlined: true,
-              height: ScreenSize.height * 0.06,
-              width: ScreenSize.width,
-              text: "Sign in with Google",
-            ),
-            const SizedBox(height: 20),
-            RichText(
-              text: TextSpan(
-                style: const TextStyle(color: Colors.black),
-                children: [
-                  const TextSpan(
-                    text: "Dont have an account? ",
-                  ),
-                  TextSpan(
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (_) => const RegisterScreen(),
+                  const SizedBox(height: 20),
+                  RichText(
+                    text: TextSpan(
+                      style: const TextStyle(color: Colors.black),
+                      children: [
+                        const TextSpan(
+                          text: "Dont have an account? ",
+                        ),
+                        TextSpan(
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => const RegisterScreen(),
+                                ),
+                              );
+                            },
+                          text: "Register",
+                          style: const TextStyle(
+                            color: kPrimaryColor,
+                            fontWeight: FontWeight.bold,
                           ),
-                        );
-                      },
-                    text: "Register",
-                    style: const TextStyle(
-                      color: kPrimaryColor,
-                      fontWeight: FontWeight.bold,
+                        ),
+                      ],
                     ),
                   ),
                 ],
